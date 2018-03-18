59e25cf67242d78f2d44cc7febcac957c19dbeda
unknown <paul_schaller@att.net>
2018-03-18 14:04:35 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+require('./routes')(app);
+
+app.use(function (err, req, res, next) {
+  console.log('We had an error.', err)
+  res.status(500).json({
+    message: err.message
+  })
+})
+
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+app.use(bodyParser.json());
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytreact";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+   useMongoClient: true
+});
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
